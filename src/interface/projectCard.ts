0368bfa63075a375e6b1ca6ab6577ee5bb50d6c5
HongTicTic <krymenghong@gmail.com>
2026-09-19 13:32:22 +0700

    add projectCard interface

---
+++ src/interface/projectCard.ts
@@ -0,0 +1,11 @@
+export interface ProjectCard {
+  id: string;
+  title: string;
+  subtitle: string;
+  coverImageUrl: string;
+  categoryId: string;
+  freelanceId: string;
+  publishedAt: string;
+  likeCount: number;
+  viewCount: number;
+}
